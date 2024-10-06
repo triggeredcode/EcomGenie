81d747dca9a0784a1b915f84754a630eb78a457c
Omkar tripathi <omkar.tripathi101@gmail.com>
2024-10-07 01:09:52 +0530

    _fixing issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ],
   define: {
     'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
   }
